02483101c5001e6c398d51da6ae5589200fd60e2
segzy2baba <Segzy2baba1@gmail.com>
2023-09-15 17:48:23 +0100

    update dashboard

--- AdminDash/src/pages/Home.jsx
+++ AdminDash/src/pages/Home.jsx
@@ -4,69 +4,77 @@ import {
   BsPeopleFill,
   // BsFillBellFill,
   BsChevronRight,
- 
 } from "react-icons/bs";
 import {
-  // BsFillArchiveFill
-  FaPiggyBank
- 
- 
+  FaPiggyBank,
 } from "react-icons/fa";
 
-// import PieChartBox from "../components/pieChartBox/PieChartBox";
+import { FcMoneyTransfer, FcUnlock } from "react-icons/fc";
+
+
 
 function Home() {
- 
-
   return (
-    
     <main className="main-container">
       <div className="main-title">
         <h3 className="dash-title">Plans</h3>
       </div>
 
       <div className="main-cards">
-
         <div className="card-content py-4">
-
           <div className="bg-white card_icon">
             <FaPiggyBank className="fs-2 pink-icon" />
           </div>
-
           <div className="card-inner mx-3">
-           <h5 className="text-dark font-weight-medium ">Personal</h5>
+            <h5 className="text-dark font-weight-medium ">Personal</h5>
             <p className="  text-muted">
               locked your savings towards long term goals
             </p>
-
           </div>
           <BsChevronRight className="fs-4" />
-          
         </div>
 
         <div className="card-content py-4">
-
           <div className="bg-white card_icon">
-          <BsPeopleFill className="fs-2 text-danger" />
+            <BsPeopleFill className="fs-2 text-danger" />
           </div>
-
           <div className="card-inner mx-3">
-           <h5 className="text-dark font-weight-medium ">Personal</h5>
+            <h5 className="text-dark font-weight-medium ">Cliq</h5>
             <p className="  text-muted">
-              locked your savings towards long term goals
+            Save towards milestones with friends.
             </p>
-
           </div>
           <BsChevronRight className="fs-4" />
-          
         </div>
 
-       
-       
-       
-      </div>
+        <div className="card-content py-4">
+          <div className="bg-white card_icon">
+            <FcMoneyTransfer className="fs-2 text-danger" />
+          </div>
+          <div className="card-inner mx-3">
+            <h5 className="text-dark font-weight-medium ">Soft</h5>
+            <p className="  text-muted">
+            Flexible savings, withdraw at any time.
+            </p>
+          </div>
+          <BsChevronRight className="fs-4" />
+        </div>
 
-    
+        <div className="card-content py-4">
+          <div className="bg-white card_icon">
+            <FcUnlock className="fs-2 text-danger" />
+          </div>
+          <div className="card-inner mx-3">
+            <h5 className="text-dark font-weight-medium ">Locked</h5>
+            <p className="  text-muted">
+            Lorem ipsum dolor sit amet si consectetur.
+            </p>
+          </div>
+          <BsChevronRight className="fs-4" />
+        </div>
+
+
+      </div>
     </main>
   );
 }
